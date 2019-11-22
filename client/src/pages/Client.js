4dc0d2996cab193b4bed6d69c8de0ea094535f07
Wadson Vaval <vavalson@hotmail.com>
2019-11-22 17:01:49 -0400

    creating profile gui

--- client/src/pages/Client.js
+++ client/src/pages/Client.js
@@ -1,6 +1,26 @@
 import React from 'react';
 const Client = props => {
-	const {id}=props.match.params;
-	return <h1>{` Hello User with id : ${id}`}</h1>;
+	const { id } = props.match.params;
+	const { Fragment } = React;
+	return (
+		<Fragment>
+			<h1>{` Hello User with id : ${id}`}</h1>
+			<div className="container">
+				<div className="d-flex text-white pb-5 border-bottom ">
+					<i className="fa fa-user fa-5x p-5 bg-primary  mr-5" />
+					<div className="text-white">
+						<h6>Nombre nombre</h6>
+						<h6>Cedula nombre</h6>
+						<h6>Fecha de Cumpleano: nombre</h6>
+						<h6>Direccion nombre</h6>
+						<h6>Referencia nombre</h6>
+						<h6>calificacion nombre</h6>
+					</div>
+				</div>
+				<h6 className="text-withe">Informaciones de indentidad</h6>
+				<h6 className="text-withe">Informaciones de contacto</h6>
+			</div>
+		</Fragment>
+	);
 };
 export default Client;
--- client/src/pages/Clients.js
+++ client/src/pages/Clients.js
@@ -3,24 +3,36 @@ import Clientes from '../component/Clients';
 const Clients = props => {
 	const [ items, setItems ] = useState([]);
 	const [ filteredClients, setFilteredClients ] = useState([]);
+	const [ err, setErr ] = useState(false);
 	useEffect(() => {
 		fetchUsers();
 	}, []);
+
 	const fetchUsers = async () => {
 		try {
 			let data = await fetch('https://jsonplaceholder.typicode.com/users');
-		data = await data.json();
-		setItems(data);
-		setFilteredClients(data);
+			data = await data.json();
+			setItems(data);
+			setFilteredClients(data);
 		} catch (error) {
-			console.log(`Error retriving the users ${error}`)
+			setErr(true);
+			console.log(`Error retriving the users ${error}`);
 		}
-		
 	};
+
 	const handleChange = e => {
 		let filter = items.filter(item => !item.name.toLowerCase().indexOf(e.target.value.toLowerCase()));
 		setFilteredClients(filter);
 	};
-	return <Clientes clients={filteredClients} handleChange={handleChange} />;
+	const { Fragment } = React;
+	return (
+		<Fragment>
+			{!err ? (
+				<Clientes clients={filteredClients} handleChange={handleChange} />
+			) : (
+				<h1 className="mt-5 pt-5 text-center text-danger">No se pudo connectarse al servidor </h1>
+			)}
+		</Fragment>
+	);
 };
 export default Clients;
